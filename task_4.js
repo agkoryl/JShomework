acf83dc99cc0def88f00762d92949024b08b9fd1
Agnieszka Koryl <ag.koryl@gmail.com>
2018-11-05 00:07:26 +0100

    New task 4 js

--- task_4.js
+++ task_4.js
@@ -1,29 +1,22 @@
-function updateTime() {
-
-var date = new Date();
-
-var hourHour = date.getHours();
-var hourMinutes = date.getMinutes();
-var hourSeconds = date.getSeconds();
-
-var hourFirst = Math.floor(hourHour/10);
-var hourTwo = hourHour - hourFirst*10;
-
-var minutesFirst = Math.floor(hourMinutes/10);
-var minutesTwo = hourMinutes - minutesFirst*10;
-
-var secondsFirst = Math.floor(hourSeconds/10);
-var secondsTwo = hourSeconds - secondsFirst*10;
-
-document.getElementById("hour1").innerText = hourFirst;
-document.getElementById("hour2").innerText = hourTwo;
-
-document.getElementById("minutes1").innerText = minutesFirst;
-document.getElementById("minutes2").innerText = minutesTwo;
-
-document.getElementById("seconds1").innerText = secondsFirst;
-document.getElementById("seconds2").innerText = secondsTwo;
-
+function setValue(id, value) {
+    document.getElementById(id).innerHTML = value;
 }
+
+function setSegmentTime (timeInterval, segmentName1, segmentName2) {
+    var firstNumber = Math.floor(timeInterval/10);
+    var secondNumber = timeInterval % 10;
+
+    setValue(segmentName1, firstNumber);
+    setValue(segmentName2, secondNumber);
+}
+
+function updateTime() {
+    var date = new Date();
+
+    setSegmentTime(date.getHours(), "hour1", "hour2");
+    setSegmentTime(date.getMinutes(), "minutes1", "minutes2");
+    setSegmentTime(date.getSeconds(), "seconds1", "seconds2");
+}
+
 updateTime();
-var clock = setInterval(updateTime, 1000);
+setInterval(updateTime, 1000);
